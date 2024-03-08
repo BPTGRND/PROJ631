6a593ca2c39b064c9b7155e30a5c5c2767050538
Baptiste Renand <BPTGRND>
2024-03-08 16:41:59 +0100

    Modification de la classe BusLine (suppression de la liste des arrêts)

--- Sujet1/src/BusLine.java
+++ Sujet1/src/BusLine.java
@@ -1,17 +1,13 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BusLine {
     private final String name;
-    private final List<BusStop> busStops;
     private final Map<String, Integer> segmentTimes;
     int departureTime;
 
     public BusLine(String name, int departureTime) {
         this.name = name;
-        this.busStops = new ArrayList<>();
         this.departureTime = departureTime;
         this.segmentTimes = new HashMap<>();
     }
@@ -20,31 +16,19 @@ public class BusLine {
         return name;
     }
 
-    public void addStop(BusStop stop) {
-        busStops.add(stop);
-    }
-
     public void addSegmentTime(BusStop fromStop, BusStop toStop, int time) {
         String segmentKey = fromStop.getName() + "-" + toStop.getName();
         segmentTimes.put(segmentKey, time);
     }
 
-    public int getSegmentTime(BusStop fromStop, BusStop toStop) {
-        String segmentKey = fromStop.getName() + "-" + toStop.getName();
-        return segmentTimes.getOrDefault(segmentKey, -1);
-    }
-
     public int getNumberOfSegments() {
-        return busStops.size() - 1;
+        return segmentTimes.size();
     }
 
     public int getTotalTime() {
         int totalTime = 0;
-        for (int i = 0; i < busStops.size() - 1; i++) {
-            BusStop fromStop = busStops.get(i);
-            BusStop toStop = busStops.get(i + 1);
-            int segmentTime = getSegmentTime(fromStop, toStop);
-            totalTime += segmentTime;
+        for (int time : segmentTimes.values()) {
+            totalTime += time;
         }
         return totalTime;
     }
@@ -52,5 +36,4 @@ public class BusLine {
     public int getArrivalTime() {
         return departureTime + getTotalTime();
     }
-
 }
--- Sujet1/src/Main.java
+++ Sujet1/src/Main.java
@@ -11,23 +11,14 @@ public class Main {
         BusStop stop5 = new BusStop("Stop5");
 
         BusLine redLine = new BusLine("RedLine", 611);
-        redLine.addStop(stop1);
-        redLine.addStop(stop2);
-        redLine.addStop(stop3);
-        redLine.addStop(stop4);
         redLine.addSegmentTime(stop1, stop2, 10);
         redLine.addSegmentTime(stop2, stop3, 10);
         redLine.addSegmentTime(stop3, stop4, 10);
 
         BusLine greenLine = new BusLine("GreenLine", 605);
-        greenLine.addStop(stop1);
-        greenLine.addStop(stop4);
         greenLine.addSegmentTime(stop1, stop4, 50);
 
         BusLine blueLine = new BusLine("BlueLine", 600);
-        blueLine.addStop(stop1);
-        blueLine.addStop(stop5);
-        blueLine.addStop(stop4);
         blueLine.addSegmentTime(stop1, stop5, 20);
         blueLine.addSegmentTime(stop5, stop4, 20);
 
